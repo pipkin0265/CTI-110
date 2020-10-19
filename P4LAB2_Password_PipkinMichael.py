67eae4b648a75580dd64d8441f468c14e2caf191
pipkin0265 <71162077+pipkin0265@users.noreply.github.com>
2020-10-18 20:50:06 -0400

    Password Modifier Lab
    Had fun with this lab!

---
+++ P4LAB2_Password_PipkinMichael.py
@@ -0,0 +1,24 @@
+word = input()
+password = ''
+
+for x in word:
+    if(x=='i'):
+        password += "!"
+        
+    elif(x=='a'):
+        password += "@"
+        
+    elif (x == 'm'):
+        password += "M"
+        
+    elif (x == 'B'):
+        password += "8"
+        
+    elif (x == 'o'):
+        password += "."
+        
+    else:
+        password += x
+        
+password += "q*s"
+print(password)
