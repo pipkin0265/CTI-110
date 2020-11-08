87711a60a1092bea352bd5fe0849fed35e7f91b1
pipkin0265 <71162077+pipkin0265@users.noreply.github.com>
2020-11-08 12:30:04 -0500

    Count Input Length

--- main[1].py
+++ main[1].py
@@ -1,6 +1,6 @@
-def miles_to_laps(user_miles):
-    return user_miles / 0.25
+x = int(input())
 
-
-if __name__ == '__main__':
-    print("{:.2f}".format(miles_to_laps(float(input()))))
+while x != 0:
+    print(x % 2, end= '')
+    x = x//2
+print()
